e47609873068c5dd45aa9a01f809c4c9acf915ab
Mike <36951165+mikelucis@users.noreply.github.com>
2021-02-01 16:06:23 +0800

    添加 URL路由；实现 URL分发。

--- boards/tests.py
+++ boards/tests.py
@@ -1,16 +1,61 @@
 from django.urls import reverse, resolve
 from django.test import TestCase
-from .views import home
+from .views import home, board_topics
+from .models import Board
 
 # Create your tests here.
 
 
 class HomeTests(TestCase):
-    def test_home_view_status_code(self):
+    # 设置测试用数据库
+    def setUp(self):
+        self.board = Board.objects.create(name='Django', description='Django board.')
         url = reverse('home')
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.response = self.client.get(url)
 
+    # 请求该URL后返回的响应状态码。状态码200（成功）
+    def test_home_view_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+        # url = reverse('home')
+        # response = self.client.get(url)
+        # self.assertEquals(response.status_code, 200)
+
+    # 将浏览器发起请求的URL与urls.py模块中列出的URL进⾏匹配
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEquals(view.func, home)
+
+    # 使⽤ assertContains ⽅法来测试 response 主体部分是否包含href
+    def test_home_view_contains_link_to_topics_page(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
+        self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
+
+
+class BoardTopicsTests(TestCase):
+    # 设置测试用数据库
+    def setUp(self):
+        Board.objects.create(name='Django', description='Django board.')
+
+    # 测试 Django 是否对于现有的 Board 返回 status code(状态码) 200(成功)
+    def test_board_topics_view_success_status_code(self):
+        url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    # 测试 Django 是否对于不存在于数据库的 Board 返回 status code 404(⻚⾯未找到)
+    def test_board_topics_view_not_found_status_code(self):
+        url = reverse('board_topics', kwargs={'pk': 99})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    # 测试 Django 是否使⽤了正确的视图函数去渲染 topics
+    def test_board_topics_url_resolves_board_topics_view(self):
+        view = resolve('/boards/1/')
+        self.assertEquals(view.func, board_topics)
+
+    # 使⽤ assertContains ⽅法来测试 response 主体部分是否包含href
+    def test_board_topics_view_contains_link_back_to_homepage(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(board_topics_url)
+        homepage_url = reverse('home')
+        self.assertContains(response, 'href="{0}"'.format(homepage_url))
--- boards/views.py
+++ boards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Board
 from django.http import HttpResponse
 
@@ -9,10 +9,19 @@ def home(request):
     boards = Board.objects.all()  # new一个对象，包括board类下所有对象
     return render(request, 'home.html', {'boards': boards})
 
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
+
+    # 老版
+    # board = Board.objects.get(pk=pk)
+    # board = Board.objects.get(pk=pk)
+    # return render(request, 'topics.html', {'board': board})
+
     # 老版
     # boards_name = list()  # 创建列表
-    # for board in boards:  # 遍历类下所有对象并添加到列表中
+    # for board in boards:  # 遍历类并添加到列表
     #     boards_name.append(boards_name)
     # response_html = '<br>'.join(boards_name)
     # return HttpResponse(response_html)
-
