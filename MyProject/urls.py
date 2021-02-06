ff5faee85f72bcd401459e41c6fbb781ee796156
Mike <36951165+mikelucis@users.noreply.github.com>
2021-02-06 15:21:19 +0800

    ADD UserControlSys import-passwdReset； ADD FormTags-function; 测试更新；

--- MyProject/urls.py
+++ MyProject/urls.py
@@ -26,7 +26,39 @@ urlpatterns = [
     url(r'^signup/$', account_views.signup, name='signup'),
     url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+
+    # 带有表单的⻚⾯，⽤于启动重置过程；
+    url(r'^reset/$',
+        auth_views.PasswordResetView.as_view(
+            template_name='password_reset.html',
+            email_template_name='password_reset_email.html',
+            subject_template_name='password_reset_subject.txt'
+        ),
+        name='password_reset'),
+
+    # ⼀个成功的⻚⾯，表示该过程已启动，指示⽤户检查其邮件⽂件夹等；
+    url(r'^reset/done/$',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='password_reset_done.html'
+        ),
+        name='password_reset_done'),
+
+    # 检查电⼦邮件发送⻚⾯中的token
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,6}-[0-9A-Za-z]{1,32})/$',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='password_reset_confirm.html'
+        ),
+        name='password_reset_confirm'),
+
+    # 告诉⽤户重置是否成功
+    url(r'^reset/complete/$',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='password_reset_complete.html'
+        ),
+        name='password_reset_complete'),
+
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
+
 ]
