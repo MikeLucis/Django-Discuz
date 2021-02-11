2c043cee46adb923c7e9c138c0b8dc6386c25361
Mikelucis <liushijie2014@hotmail.com>
2021-02-11 17:24:20 +0800

    Fix some links. ALL 91 Tests;

--- boards/templatetags/gravatar.py
+++ boards/templatetags/gravatar.py
@@ -2,7 +2,6 @@ import hashlib
 from urllib.parse import urlencode
 
 from django import template
-from django.conf import settings
 
 register = template.Library()
 
@@ -10,7 +9,7 @@ register = template.Library()
 @register.filter
 def gravatar(user):
     email = user.email.lower().encode('utf-8')
-    default = 'mm'
+    default = 'retro'
     size = 256
     url = 'https://www.gravatar.com/avatar/{md5}?{params}'.format(
         md5=hashlib.md5(email).hexdigest(),
