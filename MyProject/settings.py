f1f1ddfb5446a41d9d73e789b5294b6e82d43a77
mike <liushijie2014@hotmail.com>
2021-11-07 15:00:15 +0800

    V1.0 HistoryCommit.

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -32,6 +32,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Application definition
 
